577be9dac23fb88ceaa52ee8854b1a78cb42b086
PaulValle <paul.valle96@gmail.com>
2017-02-24 11:48:01 -0500

    sins

--- paginas/login.php
+++ paginas/login.php
@@ -21,7 +21,7 @@
 <body>
      <?php
 		  if (!isset($_SESSION['mineria'])){
-              $_SESSION['mineria']= $email;
+              $_SESSION['mineria']= "dasdas";
     ?>
         <div class="container">
         <br>
@@ -82,7 +82,7 @@
                  <br>
                  
                     <p id="profile-name" class="profile-name-card"></p>
-                    <form class="form-signin" method="post" action="validarLogin.php">
+                    <form class="form-signin" method="post" action="calidad.php">
                         
                         <span id="reauth-email" class="reauth-email"></span>
                         <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Correo Electrónico" required autofocus>
